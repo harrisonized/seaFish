f94f30f09cdc908f86e3440fff5a6c0dc0047a66
Harrison Wang <harrisonized@gmail.com>
2023-12-08 20:45:20 -0500

    update shiny layout, update dirpaths and references

--- R/shiny_umap.R
+++ R/shiny_umap.R
@@ -1,8 +1,9 @@
 ## Plots the integrated dataset
 ## Includes a dropdown menu to select the dataset and a text box to type in the gene
 
-wd = dirname(dirname(this.path::here()))  # wd = '~/github/R/harrisonRTools'
+wd = dirname(this.path::here())  # wd = '~/github/R/seaFish'
 library('shiny')
+library('ggplot2')
 suppressMessages(library('shinydashboard'))
 suppressMessages(library('Seurat'))
 library('optparse')
@@ -15,14 +16,15 @@ options(shiny.port = 3838)
 # args
 option_list = list(
     make_option(c("-i", "--input-dir"),
-                default='data/ballesteros-2020/output',
-                metavar='data/ballesteros-2020/output',
+                default='data/ballesteros-2020/output/integrated',
+                metavar='data/ballesteros-2020/output/integrated',
                 type="character",
                 help="output of scrnaseq_integrated_analysis.R"),
     
     make_option(c("-f", "--figures-dir"),
-                default="figures/scrnaseq-ballesteros/integrated",
-                metavar="figures/scrnaseq-ballesteros/integrated", type="character",
+                default="figures/ballesteros-2020/output/integrated",
+                metavar="figures/ballesteros-2020/output/integrated",
+                type="character",
                 help="set the figure directory"),
 
     make_option(c("-g", "--gene-of-interest"), default="Dnase1l1",
@@ -35,30 +37,25 @@ opt = parse_args(opt_parser)
 
 # user interface elements and layout
 ui <- fluidPage(
-    titlePanel("Ballesteros scRNAseq"),
-    sidebarLayout(
-
-        sidebarPanel(
-            sidebarMenu(
-                menuItem("Select the data",
-                         tabName = "Select the data",
-                         icon = icon("bar-chart")
-                ),
-                selectInput("dataset",
-                            "Dataset",
-                            c("bm", "lung", "pbzt", 'spleen')
-                ),
-                textInput("gene",
-                          "Gene Name",
-                          value = "Dnase1l1"
-                )
-            )
+    titlePanel("scRNAseq Data"),
+    inputPanel(
+        selectInput("dataset",
+                "Dataset",
+                c("bm", "lung", "pb", 'spleen')
         ),
-
-        mainPanel(
-            plotOutput(outputId = "fig"),  # gene expression
-            plotOutput(outputId = "img")  # clusters
-        )
+        textInput("gene",
+                "Gene Name",
+                value = "Dnase1l1"
+        ),
+        align="center"
+    ),
+    fluidRow(
+        splitLayout(
+            cellWidths = c("50%", "50%"),
+            plotOutput("clusters_img", height=600),
+            plotOutput("gene_fig", height=600)
+        ),
+        align="center",
     )
 )
 
@@ -66,36 +63,35 @@ ui <- fluidPage(
 # server-side computations
 server <- function(input, output, session) {
 
-    # plot gene expression
-    output$fig <- renderPlot({
+    # clusters
+    output$clusters_img <- renderImage({
+        filepath = file.path(
+            wd, opt[['figures-dir']],
+            paste0('umap-integrated-', input$dataset, '.png')
+        )
+        list(
+            src = filepath,
+            height = "600px", width = "750px"
+        )
+    }, deleteFile = FALSE)
+
+    # gene of interest
+    output$gene_fig <- renderPlot({
         seurat_obj <- load_rdata(
             file.path(wd, opt[['input-dir']],
-            input$dataset, 'integrated_seurat.RData')
+            paste0(input$dataset, '.RData'))
         )
         FeaturePlot(
             seurat_obj,
             reduction = "umap", 
             features = input$gene,
-            pt.size = 0.4, 
+            pt.size = 0.8, 
             order = TRUE,
-            split.by = "treatment",
+            # split.by = "treatment",
             min.cutoff = 'q10',
             label = FALSE
-        )
-    })
-
-    # plot clusters
-    output$img <- renderImage({
-        filepath = file.path(
-            wd, opt[['figures-dir']], input$dataset,
-            paste0('umap-', input$dataset, '-integrated-immgen_labeled.png')
-        )
-        list(
-            src = filepath,
-            width = "650px", 
-            height = "400px"
-        )
-    }, deleteFile = FALSE)
+        ) + ggtitle(input$gene)
+    }, height=600, width=750, scaling=1.5)
 
 }
 
